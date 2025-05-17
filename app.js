6eb48bdb0ebfd06bd19416132a9ea6c64ed5fd95
boboxoja9191 <boboxojabarnoyev010@gmail.com>
2025-05-17 13:58:15 +0500

    Apple.company
    Apple.company

---
+++ app.js
@@ -0,0 +1,8 @@
+function salom() {
+  document.getElementById("text").innerText = "Salom Apple";
+}
+addEventsListener;
+
+const wrapper = document.querySelector(".wrapper");
+
+wrapper.addEventListener("click");
